d97919a7552d2ab0fa971016a6416970808142df
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-07-15 21:00:53 -0700

    base files

---
+++ index.js
@@ -0,0 +1,28 @@
+// TODO: Include packages needed for this application
+const fs = require('fs')
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+/**
+ * Title
+ *  readme title
+ * Description
+ *  enter description
+ * Toc
+ * Installation
+ * Usage
+ * License
+ * Contributing
+ * Tests
+ * Questions
+ */
